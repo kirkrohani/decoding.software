362da5a519dfffaa1ea0c9161dbfb5836d53314a
Kirk Rohani <kirk.rohani@gmail.com>
2019-10-23 22:53:00 -0500

    Updates

---
+++ precache-manifest.2dfce920d40a6407d28ef7fa4468a084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c59d1987e07870a55d0cd190273e58",
+    "url": "/decoding.software/index.html"
+  },
+  {
+    "revision": "d4bfe4c51e9a85b2133c",
+    "url": "/decoding.software/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b5855ba281c81d54b0c2",
+    "url": "/decoding.software/static/js/2.152b8e5b.chunk.js"
+  },
+  {
+    "revision": "d4bfe4c51e9a85b2133c",
+    "url": "/decoding.software/static/js/main.f078b004.chunk.js"
+  },
+  {
+    "revision": "666fe5cf7d34c307ed35",
+    "url": "/decoding.software/static/js/runtime-main.8c9ca57b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/decoding.software/static/media/logo.25bf045c.svg"
+  }
+]);
