eea21bd320ecae3a9aaa116786c2a9fe9a2c6cfb
Kirk Rohani <kirk.rohani@gmail.com>
2019-11-14 22:24:26 -0600

    changed links in header

--- src/Theme/DesktopContainer.js
+++ src/Theme/DesktopContainer.js
@@ -57,9 +57,9 @@ class DesktopContainer extends React.Component {
                 <Menu.Item as='a' active>
                   Home
                 </Menu.Item>
-                <Menu.Item as='a'>Work</Menu.Item>
-                <Menu.Item as='a'>Company</Menu.Item>
-                <Menu.Item as='a'>Careers</Menu.Item>
+                <Menu.Item as='a'>Resume</Menu.Item>
+                <Menu.Item as='a'>Github</Menu.Item>
+                <Menu.Item as='a'>LinkedIn</Menu.Item>
                 <Menu.Item position='right'>
                   <Button as='a' inverted={!fixed}>
                     Log in
