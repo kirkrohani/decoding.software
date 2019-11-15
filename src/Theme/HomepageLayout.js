00c81a1cda0150d3f5c33b827c75c3d64d9795e6
Kirk Rohani <kirk.rohani@gmail.com>
2019-11-14 23:37:26 -0600

    added a divider to homepagelayout

--- src/Theme/HomepageLayout.js
+++ src/Theme/HomepageLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ResponsiveContainer from './ResponsiveContainer';
 import BlogSegment from './BlogSegment';
+import { Divider } from 'semantic-ui-react';
 
 
 class  HomepageLayout extends React.Component {
@@ -12,6 +13,15 @@ class  HomepageLayout extends React.Component {
                                  description=" We can give your company superpowers to do things that they never thought possible.
                                 Let us delight your customers and empower your needs... through pure data analytics."
                                 imageUrl='./images/white-image.png' />
+
+          <Divider
+            as='h4'
+            className='header'
+            horizontal
+            style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+          >
+            <a href='#'>Previous Posts</a>
+          </Divider>
       </ResponsiveContainer>
       )
   }
